75c90a29b99ad7ae97b1cdc00a7aa1583710063f
Peter Szujewski <pszujewski@gmail.com>
2019-04-13 17:01:20 -0400

    gitignore and cars program.cs

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -11,9 +11,12 @@ namespace Cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("fuel.csv");
-            foreach (var car in cars)
+
+            var query = cars.OrderByDescending(c => c.Combined);
+
+            foreach (var car in query.Take(10))
             {
-                Console.WriteLine(car.Name);
+                Console.WriteLine($"{car.Name}: {car.Combined}");
             }
         }
 
