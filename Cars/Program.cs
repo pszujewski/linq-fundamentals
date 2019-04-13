70dd7c97981f16a30b2700cf72ef90f8e8ffed92
Peter Szujewski <pszujewski@gmail.com>
2019-04-13 18:32:58 -0400

    manufacturers.csv

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -11,16 +11,39 @@ namespace Cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("fuel.csv");
+            var manufacturers = ProcessManufacturers("manufacturers.csv");
 
             var query = cars
                 .Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
                 .OrderByDescending(c => c.Combined)
                 .Select(c => c);
 
+            var hasFord = cars.Any(c => c.Manufacturer == "Ford");
+
             foreach (var car in query.Take(10))
             {
                 Console.WriteLine($"{car.Manufacturer} {car.Name}: {car.Combined}");
+                
             }
+
+            Console.WriteLine($"Does the list have any Ford cars? {hasFord}");
+        }
+
+        private static List<Manufacturer> ProcessManufacturers(string path)
+        {
+            return File.ReadAllLines(path)
+                .Where(l => l.Length > 1)
+                .Select(l =>
+                {
+                    var columns = l.Split(',');
+                    return new Manufacturer
+                    {
+                        Name = columns[0],
+                        Headquarters = columns[1],
+                        Year = int.Parse(columns[2]),
+                    };
+                })
+                .ToList();
         }
 
         private static List<Car> ProcessFile(string path)
@@ -30,6 +53,29 @@ namespace Cars
                 .Where(line => line.Length > 1)
                 .Select(Car.ParseFromCsv)
                 .ToList();
+        }
+    }
+
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(",");
+
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7]),
+                };
+            }
         }
     }
 }
